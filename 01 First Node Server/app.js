42334a6dd97b68525700a6ef6be9820e234eb77b
“heyapurv” <deskofapurv@gmail.com>
2024-11-03 15:39:28 +0530

    first commit

---
+++ 01 First Node Server/app.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    console.log(req)
+})
+
+const port = 3000
+
+
+server.listen(port, ()=>{
+    console.log(`Server running at http://localhost:${port}`)
+})
